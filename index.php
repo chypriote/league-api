16f7e85250a64f46f65ec04309810a7512540a6a
ChypRiotE <nicolastem@hotmail.fr>
2016-01-27 07:52:24 +0800

    slimmer

--- index.php
+++ index.php
@@ -1,11 +1,115 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
- 
-// Your App
-$app = new Bullet\App();
-$app->path('/', function($request) {
-    return "Hello World!";
-});
- 
-// Run the app! (takes $method, $url or Bullet\Request object)
-echo $app->run(new Bullet\Request());
+
+require 'Slim/Slim.php';
+
+$app = new Slim();
+
+$app->get('/champions', 'getChampions');
+$app->get('/champions/:id', 'getChampion');
+$app->post('/champions', 'addChampion');
+$app->put('/champions/:id', 'updateChampion');
+$app->delete('/champions/:id',	'deleteChampion');
+$app->get('/champions/search/:query', 'findChampionsByName');
+
+$app->run();
+
+function getChampions() {
+	$sql = "select * FROM champions";
+	try {
+		$db = getConnection();
+		$stmt = $db->query($sql);
+		$champions = $stmt->fetchAll(PDO::FETCH_OBJ);
+		$db = null;
+		echo json_encode($champions);
+	} catch(PDOException $e) {
+		echo '{"error":{"text":' . $e->getMessage() . '}}';
+	}
+}
+function getChampion($id) {
+	$sql = "SELECT * FROM champions WHERE id=:id";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);
+		$stmt->bindParam("id", $id);
+		$stmt->execute();
+		$champion = $stmt->fetchObject();
+		$db = null;
+		echo json_encode($champion);
+	} catch(PDOException $e) {
+		echo '{"error":{"text":'. $e->getMessage() .'}}';
+	}
+}
+function addChampion() {
+	error_log('addChampion\n', 3, '/var/tmp/php.log');
+	$request = Slim::getInstance()->request();
+	$champion = json_decode($request->getBody());
+	$sql = "INSERT INTO champions (name) VALUES (:name)";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);
+		$stmt->bindParam("name", $champion->name);
+		$stmt->execute();
+		$champion->id = $db->lastInsertId();
+		$db = null;
+		echo json_encode($champion);
+	} catch(PDOException $e) {
+		error_log($e->getMessage(), 3, '/var/tmp/php.log');
+		echo '{"error":{"text":' . $e->getMessage() . '}}';
+	}
+}
+function updateChampion($id) {
+	$request = Slim::getInstance()->request();
+	$body = $request->getBody();
+	$champion = json_decode($body);
+	$sql = "UPDATE champions SET name=:name WHERE id=:id";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);
+		$stmt->bindParam("name", $champion->name);
+		$stmt->bindParam("id", $id);
+		$stmt->execute();
+		$db = null;
+		echo json_encode($champion);
+	} catch(PDOException $e) {
+		echo '{"error":{"text":'. $e->getMessage() .'}}';
+	}
+}
+function deleteChampion($id) {
+	$sql = "DELETE FROM champions WHERE id=:id";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);
+		$stmt->bindParam("id", $id);
+		$stmt->execute();
+		$db = null;
+	} catch(PDOException $e) {
+		echo '{"error":{"text":'. $e->getMessage() .'}}';
+	}
+}
+function findChampionsByName($query) {
+	$sql = "SELECT * FROM champions WHERE LOWER(name) LIKE :query ORDER BY name";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);
+		$query = "%".$query."%";
+		$stmt->bindParam("query", $query);
+		$stmt->execute();
+		$champions = $stmt->fetchAll(PDO::FETCH_OBJ);
+		$db = null;
+		echo '{"wine": ' . json_encode($champions) . '}';
+	} catch(PDOException $e) {
+		echo '{"error":{"text":'. $e->getMessage() .'}}';
+	}
+}
+
+function getConnection() {
+	$dbhost="localhost";
+	$dbuser="root";
+	$dbpass="";
+	$dbname="league";
+	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $dbh;
+}
+
+?>
